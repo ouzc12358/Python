5724955d93cc84d290c1a2d2fdfca0baa4af0415
ouzc12358 <ouzc12358@hotmail.com>
2019-10-27 18:10:28 +0800

    import data successlly
    Signed-off-by: ouzc12358 <ouzc12358@hotmail.com>

--- sales2017.py
+++ sales2017.py
@@ -1,1 +1,8 @@
-print("sales in 2017")
+import pandas
+import numpy
+#orders=r"order.xlsx"
+sales_2017 =pandas.read_excel("order.xlsx")
+print (type(sales_2017))
+print(sales_2017.dtypes)
+print(sales_2017)
+
