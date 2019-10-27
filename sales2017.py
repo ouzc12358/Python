3f7c1e02e54441d15a7747b523be4e1b4c4a123e
ouzc12358 <ouzc12358@hotmail.com>
2019-10-27 19:07:33 +0800

    ongoing
    Signed-off-by: ouzc12358 <ouzc12358@hotmail.com>

--- sales2017.py
+++ sales2017.py
@@ -3,6 +3,15 @@ import numpy
 #orders=r"order.xlsx"
 sales_2017 =pandas.read_excel("order.xlsx")
 print (type(sales_2017))
-print(sales_2017.dtypes)
-print(sales_2017)
-
+#print(sales_2017.dtypes)
+#print(sales_2017)
+#print(sales_2017.head(300))
+#print(sales_2017.tail(50))
+print(sales_2017.columns)
+print(sales_2017.shape)
+#print(sales_2017.loc[10:30])
+SO = sales_2017["Sales Orde"]
+print(SO)
+column = ["Sales Orde","Quantity"]
+order_qty = sales_2017[column]
+print(order_qty)
