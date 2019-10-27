41e3a99846c368311e309e82a3d9c9eae9adff5e
ouzc12358 <ouzc12358@hotmail.com>
2019-10-27 21:40:07 +0800

    stuck at filtering type
    Signed-off-by: ouzc12358 <ouzc12358@hotmail.com>

--- sales2017.py
+++ sales2017.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy
+F = "FEW-CN"
 #orders=r"order.xlsx"
 sales_2017 =pandas.read_excel("order.xlsx")
 print (type(sales_2017))
@@ -13,5 +14,17 @@ print(sales_2017.shape)
 SO = sales_2017["Sales Orde"]
 print(SO)
 column = ["Sales Orde","Quantity"]
-order_qty = sales_2017[column]
+order_qty = sales_2017[column]  
 print(order_qty)
+
+col_name = sales_2017.columns.tolist()
+print(col_name)
+type_column = []
+print(type_column)
+for c in col_name:
+    if c is F:
+       type_column.append(c)
+typeofEMF = sales_2017[type_column]
+print(type_column.append(c))
+print(typeofEMF)
+print(typeofEMF.head(3))
