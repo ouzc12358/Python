07498db927a8139bc4f5654c3505e3db4266d7b2
ginwin <ouzc12358@hotmail.com>
2019-10-29 17:57:39 +0800

    nothing change

--- sales2018.py
+++ sales2018.py
@@ -1,8 +1,10 @@
 import numpy as np
-a =np.array([[0, 1 ,2, 3 ,4],
-[9,8,7,6,5]])
+a = np.array([[0, 1, 2, 3, 4], [9, 8, 7, 6]])
 print(a.ndim)
 print(a.shape)
 print(a.size)
 print(a.dtype)
 print(a.itemsize)
+
+#x = np.array(list / tuple, dtype=np.float32)
+x = np.array.()[0, 1, 2, 3]
