a3a9260ef900ed72c431e3934f005177c1e1cee3
Thomas-Tao Wang <thomas-tao.wang@cn.abb.com>
2019-12-23 22:23:01 +0800

    OK  with materail select

--- requests/SeleniumgetCX.py
+++ requests/SeleniumgetCX.py
@@ -6,9 +6,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
-import bs4 as bs
-import requests
-
 
 def clean_mark(s):
     s=s.replace('.','')
@@ -16,12 +13,30 @@ def clean_mark(s):
     return s 
 b = input("code:")
 a = clean_mark(b)
+#def disassemble(product,maincode,size,letter,language,verification,certification):
+product = a[0:3]
 maincode = a[0:6]
 size =int(a[6:9])
 letter = a[9:27]
 language = a[27:29]
 verification = a[29:31]
 certificate =a[31:34]
+
+def transformsize(_size):
+    if _size%10 == 0:
+        encode = 0.0149*_size-8.2
+        encode = round(encode)
+        _tmp = str(encode)
+        _sizecode = ".chRow:nth-child("+_tmp+") .chC2p"
+    elif _size == 651:
+        _sizecode = ".chRow:nth-child(14) .chC2p"
+    else:
+        encode = 0.011*_size+6.4755
+        encode = round(encode)
+        _tmp = str(encode)
+        _sizecode = ".chRow:nth-child("+_tmp+") .chC2p"
+    return _sizecode
+
 
 driver=webdriver.Chrome()
 ''' chrome_options = webdriver.ChromeOptions()
@@ -32,11 +47,18 @@ def login():
     #username = input("CX user-name:")
     driver.find_element_by_id("txtLogin").send_keys("Thomas-tao.wang@cn.abb.com")
     #passwd = input("Cx Password: ")
-    driver.find_element_by_id("txtPassword").send_keys("System02")
+    driver.find_element_by_id("txtPassword").send_keys("System03")
     driver.find_element_by_id("cmdLogin").click()
     driver.get("https://configurx.it.abb.com/configurx/content/DetailPopupBasket.aspx?CONF=1")
+    
     driver.find_element_by_id("cboMarket").click()
-    Select(driver.find_element_by_id("cboMarket")).select_by_visible_text("(USD) USA")
+    #market = input("please select your market form list ")
+    market = input("please enter your market:")
+    market = "(USD) "+market
+    print(market)
+    Select(driver.find_element_by_xpath("//select")).select_by_visible_text(market)
+
+    #Select(driver.find_element_by_id("cboMarket")).select_by_visible_text(market)
     driver.find_element_by_id("cboMarket").click()
     driver.find_element_by_id("cmdBasket").click()
     
@@ -47,7 +69,6 @@ def filtercode():
     #driver.find_element_by_id("Cntnt_txtFilterCode").clear()
     #maincode = input("输入主code:")
     driver.find_element_by_id("Cntnt_txtFilterCode").send_keys(maincode)
-    
     driver.find_element_by_id("Cntnt_cmdFilter").click()
     time.sleep(2)
    
@@ -58,17 +79,25 @@ def filtercode():
     
 
 def code_selection():
+    time.sleep(1)
     driver.get("https://configurx.it.abb.com/CXEngineX/Content/JModelConfiguration.aspx")
       
-    #time.sleep(1)
-    driver.find_element_by_link_text("700 : DN 700 (28 in.)").click()
     time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, ".chRow:nth-child(4) .chC2p").click()
+    driver.find_element_by_xpath("//div[@id='TabMasterContent_ddl_0']/a").click()
+    size_selection = transformsize(size) 
+    time.sleep(1)   
+    driver.find_element(By.CSS_SELECTOR, size_selection).click()
     time.sleep(1)
-    driver.find_element_by_link_text("H : Hard Rubber").click()   
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, ".chC2p").click()
-    time.sleep(1)
+    driver.find_element_by_xpath("//div[@id='TabMasterContent_ddl_1']/a").click()
+    if driver.find_element_by_xpath("//div[@id='TabMasterContent_ddl_1']/a").text[0]==letter[0]:
+        driver.find_element_by_xpath("//div[@id='TabMasterContent_ddl_1']/a").click()
+    else:
+        driver.find_element(By.CSS_SELECTOR, ".chC2p").click()
+
+
+    #driver.find_element(By.CSS_SELECTOR, ".chC2p").click()
+    #driver.find_element_by_xpath("//a[contains(text(),'K : Elastomer')]").click()
+    ''' time.sleep(1)
     driver.find_element(By.LINK_TEXT, "1 : Standard").click()
     time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, ".chSC2p").click()
@@ -84,8 +113,9 @@ def code_selection():
     driver.find_element(By.ID, "Header_cmdSave").click()
     time.sleep(3)
     coded_value=driver.find_element_by_id('Cntnt_txtCopy')
-    print(coded_value.text)
+    print(coded_value.text) '''
     
+
 if __name__ == "__main__":
     login()
     filtercode()
--- requests/testoftable.py
+++ requests/testoftable.py
@@ -1,6 +1,9 @@
 #a = "FEW315.025.A.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY"
-#b= 'FEW315.025.A.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY'
-b=input("code:")
+#b= 'FEW315.750.H.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY'
+#b= 'FEW315.801.H.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY'
+#b= 'FEW315.801.K.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY'
+#b= 'FEW315.750.H.1.S.1.A1.B.1.A.1.A.0.P.1.B.3.A.1.M5-V3.CWY'
+''' b=input("code:")
 def clean_mark(s):
     s=s.replace('.','')
     s=s.replace('-','')
@@ -18,4 +21,96 @@ print(maincode)
 
 size = int(size)
 print(size)
+ '''
+''' _size_ = [700,
+750,
+800,
+900,
+1000,
+1050,
+1100,
+1200,
+1350,
+1400,
+1500,
+1600,
+1650,
+1800,
+1950,
+2000,
+2100,
+2200,
+2400
+]
+i=0 '''
+#print(_size_[i])
+''' for i in range(len(_size_)):
+    if _size_[i]==2200:
+        size =_size_[i]+50
+    else:
+        size = _size_[i]+15
+    b=-0.000002*pow(size,2) + 0.0164*size - 8.2093
+    b=round(b)
+    print(b)
+    i=i+1 '''
+c = 2100
+i=0
+''' c = c+50
+def NodeTransformer(select):
+    d= -0.000002*pow(c,2) + 0.0164*c - 8.2093
+    d=round(d)
+    print(d)
+    d= str(d)
+    select = "chRow:nth-child("+d+") .chC2p"
+    #print(select)
+    return select
+x = NodeTransformer(c)
+print(x)
+ '''
 
+''' i = 0
+   # return product,maincode,size,letter,
+def transforomencoding(_size_):
+    
+    if _size_==2200:
+        size =_size_+50
+    else:
+        size = _size_+15
+    encode=-0.000002*pow(_size_,2) + 0.0164*_size_ - 8.2093
+    encode = round(encode)
+    select  =  str(encode)
+    size_selection = ".chRow:nth-child("+select+") .chC2p"
+    return size_selection '''
+#return maincode,size,letter,language,verification,cer
+''' def transforomencoding(_size_):
+    if _size_==2200:
+        size =_size_+50
+    else:
+        size = _size_+15
+    encode=-0.000002*pow(_size_,2) + 0.0164*_size_ - 8.2093
+    encode = round(encode)
+    select  =  str(encode)
+    size = "chRow:nth-child("+select+") .chC2p"
+    return size
+c = transforomencoding(c)
+print(c) '''
+
+def transformsize(_size):
+    if _size%10 == 0:
+        encode = 0.0149*_size-8.2
+        encode = round(encode)
+        _tmp = str(encode)
+        _sizecode = "chRow:nth-child("+_tmp+") .chC2p"
+    elif _size == 651:
+        _sizecode = "chRow:nth-child(14) .chC2p"
+    else:
+        encode = 0.011*_size+6.4755
+        encode = round(encode)
+        _tmp = str(encode)
+        _sizecode = "chRow:nth-child("+_tmp+") .chC2p"
+    return _sizecode
+
+a = 801
+
+b= transformsize(a)
+print(b)  
