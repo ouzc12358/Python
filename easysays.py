aaa0094c7520c59acee6b074a83a186ba99c834b
ouzc12358 <ouzc12358@hotmail.com>
2023-12-11 06:21:16 +0000

    5th upgrade code no errorsays
    Signed-off-by: ouzc12358 <ouzc12358@hotmail.com>

--- easysays.py
+++ easysays.py
@@ -39,8 +39,12 @@ def parse_article(content):
     try:
         soup = BeautifulSoup(content, 'html.parser')
         title = soup.find('title').text
-        paragraphs = soup.find_all('p')
-        article_content = "\n".join(paragraph.text for paragraph in paragraphs)
+        body = soup.find('body')
+        if not body:
+            raise ValueError("Body tag not found")
+
+        # Using body.text to capture all text within the body tag
+        article_content = body.text.strip()
         return title, article_content
     except Exception as e:
         logging.error(f"Error parsing article: {e}")
@@ -50,18 +54,6 @@ def append_to_word(doc, article_content, title):
     doc.add_heading(title, level=1)
     doc.add_paragraph(article_content)
     doc.add_page_break()
-
-def review_specific_articles():
-    urls = [
-        "http://paulgraham.com/greatwork.html",  # URL for "How to Do Great Work"
-        "http://paulgraham.com/super.html"       # URL for "Superlinear Returns"
-    ]
-
-    for url in urls:
-        content = fetch_page(url)
-        if content:
-            title, article_content = parse_article(content)
-            print(f"Title: {title}\nSnippet: {article_content[:500]}...\n\n")
 
 def main():
     os.makedirs('articles', exist_ok=True)
@@ -85,9 +77,6 @@ def main():
 
         doc.save(os.path.join('articles', 'all_articles.docx'))
         logging.info("All articles saved to one Word document.")
-
-    # Review specific articles
-    review_specific_articles()
 
 if __name__ == "__main__":
     main()
